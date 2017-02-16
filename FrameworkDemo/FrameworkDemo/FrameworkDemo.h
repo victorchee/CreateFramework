40e2771f96eb2d993a02402ee04b875f7a102f12
Victor Chee <victor.qhj@gmail.com>
2017-02-16 15:29:14 +0800

    Add demo project.

---
+++ FrameworkDemo/FrameworkDemo/FrameworkDemo.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkDemo.h
+//  FrameworkDemo
+//
+//  Created by Migu on 2017/2/16.
+//  Copyright © 2017年 Victor Chee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkDemo.
+FOUNDATION_EXPORT double FrameworkDemoVersionNumber;
+
+//! Project version string for FrameworkDemo.
+FOUNDATION_EXPORT const unsigned char FrameworkDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkDemo/PublicHeader.h>
+
+
